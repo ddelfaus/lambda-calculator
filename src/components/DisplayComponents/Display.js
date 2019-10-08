88ff4d110ff7c85e058b9a2cb9d64a822a12172c
ddelfaus <ddelfaus@gmail.com>
2019-10-08 18:58:27 -0400

    added CSS styles maybe MVP

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+  return (<div className="display">{/* Display any props data here */}
+    <h3>0</h3>
+    </div>
+  )
 };
+
+
+export default Display;
